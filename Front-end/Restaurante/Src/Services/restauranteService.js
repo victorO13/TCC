506521ece98cb59c52ec6ff6e64bbd0606af288f
victorO13 <50842019+victorO13@users.noreply.github.com>
2022-07-23 11:26:53 -0300

    adicionando função Update no serviceRestaurante. #29

--- Front-end/Restaurante/Src/Services/restauranteService.js
+++ Front-end/Restaurante/Src/Services/restauranteService.js
@@ -18,6 +18,24 @@ module.exports = {
             "restaurante_descricao": modelRestaurante.restaurante_descricao
         })
     },
+    async update(restauranteModel) {
+        return api.post('/restaurante/update',
+            {
+                restaurante_codigo: restauranteModel.restaurante_codigo,
+                restaurante_cnpj: restauranteModel.restaurante_cnpj,
+                restaurante_descricao: restauranteModel.restaurante_descricao,
+                restaurante_email: restauranteModel.restaurante_email,
+                restaurante_fantasia: restauranteModel.restaurante_fantasia,
+                restaurante_nome: restauranteModel.restaurante_nome,
+                restaurante_regiao: restauranteModel.restaurante_regiao,
+                restaurante_telefone: restauranteModel.restaurante_telefone
+            }
+        ).then(function (response) {
+            return response.data
+        }).catch(function (error) {
+            return error
+        })
+    },
     async findByEmail(email) {
         try {
             const response = await api.post('/restaurante/findByEmail/', {
